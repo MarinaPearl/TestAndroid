d914b76b59a3a7779df5c63d45100adc4a87aa68
marina <marina.demchuk.01@bk.ru>
2023-03-31 16:36:55 +0700

    add with rwo

--- CustomView/app/src/main/java/ru/demchuk/customview/FleBoxLayout.kt
+++ CustomView/app/src/main/java/ru/demchuk/customview/FleBoxLayout.kt
@@ -5,10 +5,7 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.marginBottom
-import androidx.core.view.marginLeft
-import androidx.core.view.marginRight
-import androidx.core.view.marginTop
+import androidx.core.view.*
 
 class FleBoxLayout @JvmOverloads constructor(
     context: Context,
@@ -19,13 +16,11 @@ class FleBoxLayout @JvmOverloads constructor(
 
     private var reaction0: View
     private var reaction1: View
-//    private var reaction2: View
+    //    private var reaction2: View
 //    private var reaction3: View
 //    private var reaction4: View
 //    private var reaction5: View
 //    private var reaction6: View
-    private val listView: ArrayList<View>
-    private var maxWidth = 0
 
     init {
         Log.d("init", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
@@ -37,123 +32,41 @@ class FleBoxLayout @JvmOverloads constructor(
 //        reaction4 = findViewById(R.id.reaction5)
 //        reaction5 = findViewById(R.id.reaction6)
 //        reaction6 = findViewById(R.id.reaction7)
-        listView =
-            arrayListOf(reaction0, reaction1)
 
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val widthMeasurePixSize = MeasureSpec.getSize(widthMeasureSpec)
         Log.d("FlexBoxLayout..............", "$widthMeasureSpec $heightMeasureSpec")
-        measureChildWithMargins(reaction0, widthMeasureSpec, 0, heightMeasureSpec, 0)
-        var widthUsed = reaction0.measuredWidth + reaction0.marginLeft + reaction0.marginLeft
-        var heightUsed = reaction0.measuredHeight + reaction0.marginTop + reaction0.marginBottom
-        measureChildWithMargins(
-            reaction1,
-            widthMeasureSpec,
-            widthUsed,
-            heightMeasureSpec,
-            heightUsed
-        )
-        widthUsed += reaction1.measuredWidth + reaction1.marginLeft + reaction1.marginLeft
-        heightUsed = reaction1.measuredHeight + reaction1.marginTop + reaction1.marginBottom
-        measureChildWithMargins(
-            reaction2,
-            widthMeasureSpec,
-            widthUsed,
-            heightMeasureSpec,
-            heightUsed
-        )
-        widthUsed += reaction2.measuredWidth + reaction2.marginLeft + reaction2.marginLeft
-        heightUsed = reaction2.measuredHeight + reaction2.marginTop + reaction2.marginBottom
-        measureChildWithMargins(
-            reaction3,
-            widthMeasureSpec,
-            widthUsed,
-            heightMeasureSpec,
-            heightUsed
-        )
-        widthUsed += reaction3.measuredWidth + reaction3.marginLeft + reaction3.marginLeft
-        heightUsed = reaction3.measuredHeight + reaction3.marginTop + reaction3.marginBottom
-        measureChildWithMargins(
-            reaction4,
-            widthMeasureSpec,
-            widthUsed,
-            heightMeasureSpec,
-            heightUsed
-        )
-        widthUsed += reaction4.measuredWidth + reaction4.marginLeft + reaction4.marginLeft
-        heightUsed = reaction4.measuredHeight + reaction4.marginTop + reaction4.marginBottom
-        measureChildWithMargins(
-            reaction5,
-            widthMeasureSpec,
-            widthUsed,
-            heightMeasureSpec,
-            heightUsed
-        )
-        widthUsed += reaction5.measuredWidth + reaction5.marginLeft + reaction5.marginLeft
-        heightUsed = reaction5.measuredHeight + reaction5.marginTop + reaction5.marginBottom
-        measureChildWithMargins(
-            reaction6,
-            widthMeasureSpec,
-            widthUsed,
-            heightMeasureSpec,
-            heightUsed
-        )
-        var totalWidth = paddingLeft + paddingRight
-        var totalHeight = paddingBottom + paddingTop
-        maxWidth = 0
+        var widthUsed = 0
+        var heightUsed = 0
         var maxHeight = 0
-        for (i in 0 until listView.size) {
-            if (totalWidth + listView[i].measuredWidth +
-                listView[i].marginLeft + listView[i].marginRight <= widthMeasureSpec
-            ) {
-                totalWidth += listView[i].measuredWidth +
-                        listView[i].marginLeft + listView[i].marginRight
-                maxWidth = maxOf(maxWidth, totalWidth)
-                maxHeight = maxOf(
-                    maxHeight, listView[i].measuredHeight +
-                            listView[i].marginBottom + listView[i].marginTop
-                )
-            } else {
-                totalHeight += maxHeight
-                maxHeight = 0
-                totalWidth = 0
-            }
+        children.forEach {
+            measureChildWithMargins(it, widthMeasureSpec, widthUsed, heightMeasureSpec, heightUsed)
+            widthUsed += it.measuredWidth + it.marginLeft + it.marginRight + it.paddingLeft + it.paddingRight
+            heightUsed += it.measuredHeight + it.marginTop + it.marginBottom + it.paddingBottom + it.paddingTop
+            maxHeight = maxOf(
+                maxHeight,
+                it.measuredHeight + it.marginTop + it.marginBottom + it.paddingBottom + it.paddingTop
+            )
         }
-        setMeasuredDimension(maxWidth, totalHeight)
+        widthUsed += paddingLeft + paddingRight + marginLeft + marginRight
+        maxHeight += paddingTop + paddingBottom + marginTop + marginBottom
+        setMeasuredDimension(widthUsed, maxHeight)
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        var offsetX = paddingLeft + reaction1.marginLeft
-        var offsetY = paddingTop + reaction1.marginTop
-        var maxHeight = 0
-        for (i in 0 until listView.size) {
-            if (offsetX + listView[i].measuredWidth <= maxWidth) {
-                listView[i].layout(
-                    offsetX,
-                    offsetY,
-                    offsetX + listView[i].measuredWidth,
-                    offsetY + listView[i].measuredHeight
-                )
-            } else {
-                offsetX = paddingLeft + listView[i].marginLeft
-                offsetY = paddingTop + listView[i].marginTop + maxHeight
-                maxHeight = 0
-                listView[i].layout(
-                    offsetX,
-                    offsetY,
-                    offsetX + listView[i].measuredWidth,
-                    offsetY + listView[i].measuredHeight
-                )
-            }
-            offsetX += listView[i].measuredWidth + listView[i].marginRight
-            if (i != listView.size - 1) {
-                offsetX += listView[i + 1].marginLeft
-            }
-            maxHeight = maxOf(
-                maxHeight,
-                listView[i].measuredHeight + listView[i].marginTop + listView[i].marginBottom
+        var offsetX = paddingLeft + reaction0.marginLeft + marginLeft
+        var offsetY = paddingTop + reaction0.marginTop +marginTop
+        children.forEach {
+            offsetX += it.marginLeft
+            it.layout(
+                offsetX,
+                offsetY + it.marginTop,
+                offsetX + it.measuredWidth,
+                offsetY + it.measuredHeight + it.marginTop
             )
+            offsetX += it.measuredWidth + it.marginRight
         }
 
     }
@@ -173,4 +86,6 @@ class FleBoxLayout @JvmOverloads constructor(
     override fun checkLayoutParams(p: LayoutParams?): Boolean {
         return p is MarginLayoutParams
     }
+
+
 }
--- CustomView/app/src/main/java/ru/demchuk/customview/MainActivity.kt
+++ CustomView/app/src/main/java/ru/demchuk/customview/MainActivity.kt
@@ -1,18 +1,28 @@
 package ru.demchuk.customview
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.view.children
 import ru.demchuk.customview.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        binding.reactionView.setOnClickListener {
-//            it.isSelected = !it.isSelected
-//        }
+        binding.flexBox.children.forEach {
+            it.setOnClickListener { reaction ->
+                reaction.isSelected = !reaction.isSelected
+                Log.d("aaaaaaaaaaaaaa", "${it.attributeSourceResourceMap[2]}")
+
+            }
+        }
 
     }
 }
